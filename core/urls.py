e247478d4f9bf6f92d6961629a4b763c9f8f0019
Sparsh1101 <sparshtgupta@gmail.com>
2021-04-20 16:04:10 +0530

    Added project code

---
+++ core/urls.py
@@ -0,0 +1,30 @@
+from django.urls import path
+from .views import (
+    ItemDetailView,
+    CheckoutView,
+    HomeView,
+    AllOrdersView,
+    OrderSummaryView,
+    add_to_cart,
+    remove_from_cart,
+    remove_single_item_from_cart,
+    PaymentView,
+    RequestRefundView
+)
+
+app_name = 'core'
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('checkout/', CheckoutView.as_view(), name='checkout'),
+    path('all-orders/', AllOrdersView.as_view(), name='all-orders'),
+    path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
+    path('product/<slug>/', ItemDetailView.as_view(), name='product'),
+    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
+    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
+    path('remove-item-from-cart/<slug>/', remove_single_item_from_cart,
+         name='remove-single-item-from-cart'),
+    path('payment/<payment_option>/', PaymentView.as_view(), name='payment'),
+    path('request-refund/<slug>/',
+         RequestRefundView.as_view(), name='request-refund')
+]
