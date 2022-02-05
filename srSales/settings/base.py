3363f2fa2a4d0d9fbb67d37ef6699dbc01418416
Sparsh1101 <sparshtgupta@gmail.com>
2022-02-05 16:00:37 +0530

    Fix Error

--- srSales/settings/base.py
+++ srSales/settings/base.py
@@ -5,6 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__))))
 
 SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'secret_key'
 
 INSTALLED_APPS = [
     'django.contrib.admin',
