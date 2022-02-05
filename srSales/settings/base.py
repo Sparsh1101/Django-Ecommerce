9bb71518888000971ede2323791cb42f3fb58200
Sparsh1101 <sparshtgupta@gmail.com>
2022-02-05 16:03:54 +0530

    Fix Error

--- srSales/settings/base.py
+++ srSales/settings/base.py
@@ -4,7 +4,6 @@ from decouple import config
 BASE_DIR = os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__))))
 
-SECRET_KEY = config('SECRET_KEY')
 SECRET_KEY = 'secret_key'
 
 INSTALLED_APPS = [
